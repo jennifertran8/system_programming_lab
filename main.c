8e21b394c10b28a0e36e33c919a35d24e9a74802
jennifertran8 <60623121+jennifertran8@users.noreply.github.com>
2020-02-05 12:18:50 -0600

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int​ main​(​void) {
-  int distance = 100;
-  float power = 2.345;
-  double supper_power = 51.87;
-  char initial = 'V';
-  char first_name [] = "Lam";
-  char last_name [] = "Tran";
- ​
-  printf("enter your first name: %s \n", first_name);
-  printf("enter your initial: %c \n", initial);
-  printf("enter your last name: %s \n", last_name);
-  printf("enter your whole name: %s %c %s \n", first_name, initial, last_name);
+int main(int argc, char *argv[]) {
+  int i = 0;
 
-  printf("you are %d miles", distance);
-  printf​(​"You have %f levels of power.\n"​,​ power​); 
-  printf​(​"You have %f awesome super powers.\n"​,​ super_power​); 
+  if (argc == 1) {
+    printf("you are only have one argument. you suck.\n");
+  }
+  else if (argc > 1 && argc < 4)
+  {
+    printf("here is your argumemts: \n");
+    for (i=0; i<argc; i++){
+      printf("%s \n", argv[i]);
+    }
+  }
+  else{
+    printf("you have to many arguments. you suck.\n");
+  }
   return 0;
-}
+  }
+
+ 
